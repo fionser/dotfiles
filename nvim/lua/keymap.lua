d4107f35db2b31256e99a58729c55d8e2ba19d51
juhou.lwj <juhou.lwj@antgroup.com>
2024-01-17 22:26:43 +0800

    update

--- nvim/lua/keymap.lua
+++
@@ -1,9 +0,0 @@
--- nvim-tree
-vim.api.nvim_set_keymap("n", "<Leader><Space>", "<cmd>NvimTreeToggle<cr>", { noremap = true, silent = true })
-
--- File Find
-local tel_builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", tel_builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", tel_builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", tel_builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", tel_builtin.help_tags, {})
